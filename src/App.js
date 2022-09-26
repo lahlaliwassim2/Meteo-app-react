4c8206a2ddd18829564645b58b6415b8f151cad8
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-26 15:22:33 +0100

    consomation de l api qui contien un objet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 const App = () => {
   const fetchData = async()=>{
-      axios.get("api.openweathermap.org/data/2.5/weather?q=Safi&appid=267e5fef0d87a3de88fae3ffa91265ca"
+     await axios.get("https://api.openweathermap.org/data/2.5/weather?q=Safi&appid=267e5fef0d87a3de88fae3ffa91265ca"
       )
-      .then((res)=>console.log(res.data))
-      
-      
+      .then((res)=>console.log(res.data))    
   }
+  useEffect(()=>{
+    fetchData();
+  },[]);
   return (
     <>
     <div className='container app'>
