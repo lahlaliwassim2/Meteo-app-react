d0b7e87ef43a908c0c78734483cfa08f2419fea1
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-26 15:08:59 +0100

    ajouter axios pour la consomation d API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
-
+import axios from 'axios'
 const App = () => {
+  const fetchData = async()=>{
+      axios.get("api.openweathermap.org/data/2.5/weather?q=Safi&appid=267e5fef0d87a3de88fae3ffa91265ca"
+      )
+      .then((res)=>console.log(res.data))
+      
+      
+  }
   return (
     <>
     <div className='container app'>
