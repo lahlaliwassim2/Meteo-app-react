2f0700816f706fd26a60c2974081e19c4e9186e4
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-26 14:44:38 +0100

    ajouter le component principale

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='container app'>
+      <div className='app_title'>
+      <h2>Méteo de votre ville </h2>
+      </div>
+    </div>
+ </> )
+}
+
+export default App
