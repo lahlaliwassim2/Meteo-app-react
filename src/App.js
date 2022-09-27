5396b6f47f35c4aae5919b4e4e173639795ea346
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-27 13:41:11 +0100

    ajouter la partie de temperature et la recupere de l objet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const App = () => {
-  const fetchData = async()=>{
-     await axios.get("https://api.openweathermap.org/data/2.5/weather?q=Safi&appid=267e5fef0d87a3de88fae3ffa91265ca"
+      const  [Data, setData] = useState("");
+  const fetchData = async() => {
+      await axios.get("https://api.openweathermap.org/data/2.5/weather?q=Safi&appid=267e5fef0d87a3de88fae3ffa91265ca"
       )
-      .then((res)=>console.log(res.data))    
-  }
+      .then((res)=> setData(res.data))    
+  };
+  
   useEffect(()=>{
     fetchData();
   },[]);
+  console.log(Data)
   return (
     <>
     <div className='container app'>
       <div className='app_title'>
       <h2>Méteo de votre ville </h2>
+      <input type="shearch" placeholder='entrer la ville '/>
+     <div className='btn-shear d-flex justify-content-end my-3'> <button className='btn btn-primary'> rechercher </button></div>
+     <hr />
+     <div className='d-flex justify-content-center aligh-items-center'>
+      {Data ? <><h4>{Data.main.temp}</h4> C</> : "entrer une autre ville"}
+     </div>
       </div>
     </div>
+    
  </> )
 }
 
