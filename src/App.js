c3a0ca1d6d7cdace2ced8f13f702bad9c3327b64
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-28 17:15:24 +0100

    rendre les parties qui affiche le degrée de temperature et la vitesse de vent et le degrée d'humiditée dynamique

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { FiWind } from "react-icons/fi"
 import { BsFillSunFill } from "react-icons/bs"
 
 
+const meteoImg = new URL("../public/img/meteo.jpg" , import.meta.url)
+
 
 const App = () => {
   const [Data, setData] = useState("");
@@ -26,7 +28,14 @@ const App = () => {
   console.log(Data)
   return (
     <>
-      <div className='container app'>
+       
+<div className='cont'>
+        
+      
+     
+      <h3 className='  d-flex justify-content-end p-3 youc'><span className='w1'>Y</span><span className='w2'>o</span><span className='w3'>u</span><span className='w4'>W</span><span className='w5'>e</span><span className='w6'>a</span><span className='w7'>t</span><span className='w8'>h</span><span className='w9'>e</span><span className='w10'>r</span></h3>
+      <div className='container app d-flex flex-colomn'>
+     
         <div className='app_title'>
           <h2>Méteo de votre ville </h2>
           <input type="shearch" placeholder='entrer la ville '
@@ -44,18 +53,18 @@ const App = () => {
               C
               </h1>
               <div className='row'>
-                <div className='d-flex col-lg-4 col-4 flex-column'>Prossure <BsFillSunFill /> :{Data.main.pressure}
+                <div className='d-flex col-lg-4 col-4 flex-column'>Prossure :<BsFillSunFill /> {Data.main.pressure}
                 </div>
-                <div className='col-lg-4 d-flex col-4 flex-column'>Humiditée <WiHumidity />:<br></br>{Data.main.humidity}</div>
+                <div className='col-lg-4 d-flex col-4 flex-column'>Humiditée:<WiHumidity />{Data.main.humidity}</div>
                 <div className='col-lg-4 col-4 d-flex flex-column'>Vent: <FiWind />{Data.wind.speed}KM/h </div>
               </div>
               <div className='d-flex justify-content-center h1'>{Data.name}</div>
             </>
-              : "entrer une autre ville"}
+              :"entrer une autre ville"}
           </div>
         </div>
       </div>
-
+</div>
     </>)
 }
 
